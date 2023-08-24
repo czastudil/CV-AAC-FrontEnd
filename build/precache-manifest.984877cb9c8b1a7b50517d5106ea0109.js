4be6b2eea696b1abcc367db396de536cfc53b6a8
Zastudil, Cindy <crzastudil@gmail.com>
2023-08-24 15:06:05 -0400

    add build scripts

---
+++ build/precache-manifest.984877cb9c8b1a7b50517d5106ea0109.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d99b7c824ae5649351ea6a1af6e2e0a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3497435f86552f54b0fe",
+    "url": "/static/css/main.05a74f43.chunk.css"
+  },
+  {
+    "revision": "e8e6cda1777bcb9263a0",
+    "url": "/static/js/2.5d00fa33.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.5d00fa33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3497435f86552f54b0fe",
+    "url": "/static/js/main.66b2fa3e.chunk.js"
+  },
+  {
+    "revision": "bd7fad4a94ca8199d7d7",
+    "url": "/static/js/runtime-main.0d8844c5.js"
+  },
+  {
+    "revision": "e22e64e0e28bc98f8b3289b1540ca6c9",
+    "url": "/static/media/play-button.e22e64e0.png"
+  },
+  {
+    "revision": "22031e38e2dfb215f1f0015515f6391b",
+    "url": "/static/media/trash-bin.22031e38.png"
+  }
+]);
